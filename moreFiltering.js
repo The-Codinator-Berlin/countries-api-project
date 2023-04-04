4000d70fda37e0872cfa04f70a11210359851904
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-04-04 12:41:14 +0200

    Cards created successfully

--- moreFiltering.js
+++ moreFiltering.js
@@ -1,31 +1,34 @@
-// const cards = document.getElementById("cardContainer");
 
-// for (let i = 0; i < data.length; i++) {
-//   let card = document.createElement("div");
-
-//   let flag = document.createElement("img");
-//   flag.src = data[i].flags.png;
-
-//   let x = document.getElementById("cardButton");
-//   let buttons = document.createElement("BUTTON", "More");
-
-//   let h5 = document.getElementById("h-5");
-//   let header = document.createElement("h5");
-//   header.innerText = data[i].name.common;
-
-//   cardContainer.appendChild(div);
-//   cardContainer.appendChild(img);
-//   cardContainer.appendChild(BUTTON, "More");
-//   cardContainer.appendChild(h5);
-// }
-
-
-for (let i = 0; i < data.length; i++) {
+function createCards() {
+  for (let i = 0; i < data.length; i++) {
     let container = document.getElementById("container");
 
     let cardDiv = document.createElement("div");
     cardDiv.classList.add("card");
     cardDiv.setAttribute("style", "width: 18rem;");
 
-    container.appendChild(cardDiv)
+    let img = document.createElement("img");
+    img.classList.add("card-img-top");
+    img.setAttribute("src", data[i].flags.png);
+    img.setAttribute("alt", data[i].flags.alt);
+
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    let imgTitle = document.createElement("h5");
+    imgTitle.classList.add("card-title");
+    imgTitle.innerText = data[i].name.common;
+
+    let btn = document.createElement("button");
+    btn.classList.add("btn");
+    btn.classList.add("btn-primary");
+    btn.innerText = "Click for more information";
+
+    container.appendChild(cardDiv);
+    cardDiv.appendChild(img);
+    cardDiv.appendChild(cardBody);
+    cardBody.appendChild(imgTitle);
+    cardBody.appendChild(btn);
+  }
 }
+createCards();
