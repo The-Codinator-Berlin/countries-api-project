6db6b7cb6171053d2fea57e7beb5ff4c37295f36
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-04-08 08:27:26 +0200

    Fetch now working and cards generating live.

--- moreFiltering.js
+++ moreFiltering.js
@@ -1,6 +1,24 @@
+const url = "https://restcountries.com/v3.1/all";
 
-function createCards() {
-  for (let i = 0; i < data.length; i++) {
+const fetchData = () => {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((countriesResult) => {
+      const allData = countriesResult;
+      createCards(allData);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+fetchData();
+
+//This function creates a card for each country with the img, name of country and a button//
+function createCards(allData) {
+  for (let i = 0; i < allData.length; i++) {
     let container = document.getElementById("container");
 
     let cardDiv = document.createElement("div");
@@ -9,15 +27,15 @@ function createCards() {
 
     let img = document.createElement("img");
     img.classList.add("card-img-top");
-    img.setAttribute("src", data[i].flags.png);
-    img.setAttribute("alt", data[i].flags.alt);
+    img.setAttribute("src", allData[i].flags.png);
+    img.setAttribute("alt", allData[i].flags.alt);
 
     let cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
 
     let imgTitle = document.createElement("h5");
     imgTitle.classList.add("card-title");
-    imgTitle.innerText = data[i].name.common;
+    imgTitle.innerText = allData[i].name.common;
 
     let btn = document.createElement("button");
     btn.classList.add("btn");
@@ -31,4 +49,4 @@ function createCards() {
     cardBody.appendChild(btn);
   }
 }
-createCards();
+
