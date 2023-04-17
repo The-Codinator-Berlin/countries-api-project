443406915fb47005d2d28c49e5fbfc391e2ad08b
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-04-17 16:37:47 +0200

    Simplified process for fetch and export

--- getCountriesData.js
+++ getCountriesData.js
@@ -1,16 +1,18 @@
-const getCountriesData = (nextStepFunc) => {
+// Fetch function ------------------------------------------>
+
+const getCountries = async () => {
   const url = "https://restcountries.com/v3.1/all";
 
-  fetch(url)
+  return fetch(url)
     .then((response) => {
       return response.json();
-    })
-    .then((countriesResult) => {
-      nextStepFunc(countriesResult);
     })
     .catch((error) => {
       console.log(error);
     });
 };
 
+const getCountriesData = await getCountries();
+
+// Exporting fetch ------------>
 export { getCountriesData };
