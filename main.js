19799d6639cd5d40c6dfddbde5831e78048e79c6
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-04-17 16:40:05 +0200

    Simplified process with fetch results

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
+// Fetch import ------------------------------------------>
 import { getCountriesData } from "./getCountriesData.js";
 
-// Fetch function -----------------------------------------------------
-getCountriesData(createTableData);
-
-// Creating table with data ------------------------------------------
+const results = getCountriesData;
+// console.log("results", results);
 
 const tableBody = document.getElementById("tableBody");
 
-function createTableData(countries) {
+// Creating table filled dynamically with data from API ------>
+function createTableData(results) {
   tableBody.innerText = "";
 
-  for (let i = 0; i < countries.length; i++) {
+  for (let i = 0; i < results.length; i++) {
     const row = document.createElement("tr");
 
     let flag = document.createElement("img");
-    flag.src = countries[i].flags.png;
+    flag.src = results[i].flags.png;
 
     let names = document.createElement("td");
-    names.innerText = countries[i].name.common;
+    names.innerText = results[i].name.common;
 
     let capital = document.createElement("td");
-    capital.innerText = countries[i].capital;
+    capital.innerText = results[i].capital;
 
     let population = document.createElement("td");
-    population.innerText = countries[i].population;
+    population.innerText = results[i].population;
 
     row.appendChild(flag);
     row.appendChild(names);
@@ -33,6 +33,8 @@ function createTableData(countries) {
     tableBody.appendChild(row);
   }
 }
+
+createTableData(results);
 
 // Dropdown/ Event listener------------------------------------------------------------
 
@@ -44,14 +46,14 @@ function optionSelect() {
   const region = this.value;
   // this.value mean when someone is selecting this value or this dropdown option
   const filteredCountries = [];
-  // An empty array called filteredData is created to hold the countries that match the selected region.
+  // An empty array called filteredCounties is created to hold the countries that match the selected region.
   if (region === "All") {
-    createTableData(allData);
-    // creates table with all data 
+    createTableData(results);
+    // creates table with results
   } else {
-    for (let i = 0; i < allData.length; i++) {
-      if (allData[i].region === region) {
-        filteredCountries.push(allData[i]);
+    for (let i = 0; i < results.length; i++) {
+      if (results[i].region === region) {
+        filteredCountries.push(results[i]);
         // OR loops through and takes just the data that matches the option selected
       }
     }
